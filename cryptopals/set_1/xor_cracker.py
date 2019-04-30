2ab3bd74651594720d3503aa49075a30e7a7ccde
slopey112 <howard_peng@icloud.com>
2019-04-30 14:29:50 -0700

    added CLI to xor cracker

--- cryptopals/set_1/xor_cracker.py
+++ cryptopals/set_1/xor_cracker.py
@@ -9,6 +9,9 @@ letter_frequency = {
     'v': 0.0082903, 'w': 0.0171272, 'x': 0.0013692, 'y': 0.0145984, 'z': 0.0007836, ' ': 0.1918182
 }
 
+CIPHER_TEXT = ''
+REGISTERS = {}
+
 def hamming_dist(str1, str2):
     assert (len(str1) == len(str2)), "Lengths of strings must be equal"
     dist = 0
@@ -22,7 +25,7 @@ def hamming_dist(str1, str2):
 def find_keysize(cipher, answers):
     assert (answers <= len(cipher)), "Number of answers must be less than or equal to the length of the cipher"
     map = {}
-    for i in range(2, 40):
+    for i in range(2, 50):
         blocks = []
         blocks.append(cipher[:i])
         blocks.append(cipher[i:i*2])
@@ -129,34 +132,177 @@ def crack_cipher(cipher, answers = 1):
     }
 
 def load_file():
+    global CIPHER_TEXT, REGISTERS
+    print()
+    print('Enter file path:')
+    path = input()
+    try:
+        with open(path) as f:
+            cipher = b64decode(f.read()).decode('ascii')
+    except FileNotFoundError:
+        print('Sorry. File not found.')
+        return
+    CIPHER_TEXT = cipher
+    return
+
+def manual_load():
+    global CIPHER_TEXT
+    print()
+    print('Enter the cipher text. Express bytes as usual (EX \\x41):')
+    try:
+        cipher = input().encode('utf-8').decode('unicode_escape')
+    except UnicodeDecodeError:
+        print('Error with byte input.')
+        return
+    CIPHER_TEXT = cipher
+    return
+
+def process_crack_cipher():
+    global REGISTERS
+    print()
+    if not CIPHER_TEXT:
+        print('No cipher text loaded. Load a file or text first.')
+        return
+    print('Enter number of keys to search:')
+    try:
+        selection = int(input())
+    except ValueError:
+        print('Invalid search space.')
+        return
+    print('Cipher cracking in progress... (This may take a while for longer cipher texts)')
+    try:
+        REGISTERS = crack_cipher(CIPHER_TEXT, answers = selection)
+    except AssertionError:
+        print('Invalid search space.')
+        return
+    print('Cracking complete.')
+    return
+
+def view_keys():
+    print()
+    header = '-' * 79
+    print(header)
+    print('{:<5} {:<50} {:<5} {:<5}'.format('ID', 'KEY', 'SCORE', 'CHANCE'))
+    print(header)
+    for i in range(len(REGISTERS['keys'])):
+        print('{:>5} {:<50} {:>6} {:>5}'.format(i, REGISTERS['keys'][i], round(REGISTERS['scores'][REGISTERS['keys'][i]], 1), round(REGISTERS['chance'][REGISTERS['keys'][i]] * 100, 1)))
+    print(header)
+    return
+
+def view_decrypted_cipher():
+    print()
+    print('Enter id of key:')
+    try:
+        key = int(input())
+    except ValueError:
+        print('Invalid key id.')
+        return
+    try:
+        text = REGISTERS['text'][REGISTERS['keys'][key]]
+    except IndexError:
+        print('Invalid key id.')
+        return
+    print('-------------BEGIN DECODED MESSAGE----------------')
+    print(text)
+    print('--------------END DECODED MESSAGE-----------------')
+    return
+
+def view_registers():
+    if not REGISTERS:
+        print('Registers not loaded. Crack a cipher first.')
+        return
     while 1:
-        
+        print()
+        print('Register Selection')
+        print('1: Cipher text')
+        print('2: Keys')
+        print('3: Decrypted cipher')
+        print('4: Exit')
+        try:
+            selection = int(input())
+        except ValueError:
+            print('Invalid selection.')
+            continue
+        if selection < 1 or selection > 5:
+            print('Invalid selection.')
+            continue
+        elif selection == 1:
+            print(CIPHER_TEXT)
+        elif selection == 2:
+            view_keys()
+        elif selection == 3:
+            view_decrypted_cipher()
+        elif selection == 4:
+            print()
+            break
+
+def help_menu():
+    print()
+    print('Menu options')
+    print()
+    print('Load file:')
+    print('Enter the path of where the file is. Make sure the contents of the file are in base 64.')
+    print('This function takes the file contents, decodes the base 64, and derives the cipher from')
+    print('the encoded file.')
+    print()
+    print('Manual load:')
+    print('Manually enter string that is the cipher. Bytes can be expressed per normal python')
+    print('syntax.')
+    print()
+    print('Crack cipher:')
+    print('After the cipher is loaded, call this function to crack it and the results will be sent')
+    print('to the registers.')
+    print()
+    print('View registers')
+    print('This function prints the results of the cipher cracking in pretty print. You can see the')
+    print('original ciphertext, the cracked keys and their associated probabilities, as well as the')
+    print('decoded ciphertext.')
+    return
 
 def main():
-    print('\    /   -----      -----')
-    print(' \  /   |     |    |     |')
-    print('  \/    |     |     -----')
-    print('  /\    |     |    |   \')
-    print(' /  \   |     |    |    \')
-    print('/    \   -----     |     |')
+    print('\    /   -----     -----')
+    print(' \  /   |     |   |     |')
+    print('  \/    |     |    -----')
+    print('  /\    |     |   |   \ ')
+    print(' /  \   |     |   |    \ ')
+    print('/    \   -----    |     |')
     print('9fh9#$1jf#CRACKERF59j3$18hf#')
     print()
     while 1:
+        print()
         print('MENU')
         print('1: Load file')
         print('2: Manual load')
         print('3: Crack cipher')
         print('4: View registers')
-        print('5: Quit')
-        print()
+        print('5: Help')
+        print('6: Quit')
         try:
             selection = int(input())
         except ValueError:
+            print('Not a valid selection.')
             continue
-        if selection > 5 or selection < 1:
+        if selection > 6 or selection < 1:
+            print('Not a valid selection.')
             continue
-        if selection == 1:
-
+        elif selection == 1:
+            load_file()
+        elif selection == 2:
+            manual_load()
+        elif selection == 3:
+            process_crack_cipher()
+        elif selection == 4:
+            view_registers()
+        elif selection == 5:
+            help_menu()
+        elif selection == 6:
+            print()
+            print('Goodbye.')
+            break
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print('Goodbye.')
