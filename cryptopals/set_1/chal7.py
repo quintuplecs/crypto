d94ec97d9f3e829b27bd55a65c5a7c2f478a920b
slopey112 <howard_peng@icloud.com>
2019-04-30 15:21:59 -0700

    solve chal7

---
+++ cryptopals/set_1/chal7.py
@@ -0,0 +1,10 @@
+from Crypto.Cipher import AES
+from base64 import b64decode
+
+key = b'YELLOW SUBMARINE'
+
+with open('../../data/aes_ciphertext') as f:
+    cipher_text = b64decode(f.read())
+
+cipher = AES.new(key, AES.MODE_ECB)
+print(cipher.decrypt(cipher_text).decode('ascii'))
